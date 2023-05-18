73b4f1469a07dcb67d23c4baa5a8d38115e38324
duongsaikik <87181525+duongsaikik@users.noreply.github.com>
2023-05-18 15:40:11 +0700

    update base url

--- client/util/fetcher.js
+++ client/util/fetcher.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/api'
-    : `https://${process.env.SITE_NAME}/api`
+const baseURL = 'https://stack-question-ba.onrender.com/api'
 const headers = { 'Content-Type': 'application/json' }
 const publicFetch = axios.create({
   baseURL,
